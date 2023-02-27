0191cb1eb50162e92f42ac53b25c5607252aa30c
Michal Stachura <michal.stachura@santri.pl>
2023-02-27 12:25:30 +0100

    initioal commit

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="mt-14 pb-14">
+      <div class="container mx-auto p-4">
+        <NuxtPage />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+  const images = ref([
+    '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'
+  ])
+</script>
+
+<template>
+  <div>
+    <h1>Homepage</h1>
+    <div class="grid grid-cols-2 lg:grid-cols-4 gap-2">
+      <div
+      v-for="image in images"
+      >
+        <nuxt-img
+          :src="`/images/${image}`"
+          loading="lazy"
+        />
+      </div>
+    </div>
+  </div>
+</template>
