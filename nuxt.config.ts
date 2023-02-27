06bb4fc574d9633844826f6799ff5ece62e518cb
Michal Stachura <michal.stachura@santri.pl>
2023-02-27 12:45:53 +0100

    Add workaround

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      imagesUrl: process.env.IMAGES_URL || ''
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/image-edge',
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+  const config = useRuntimeConfig();
   const images = ref([
     '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'
   ])
@@ -6,13 +7,32 @@
 
 <template>
   <div>
-    <h1>Homepage</h1>
+    <p>This images works only locally. On netlify image source is:</p>
+      <pre class="bg-gray-800 p-4 my-4 rounded text-white">
+        e.g. src="/_ipx/_/images/1.jpg"
+      </pre>
     <div class="grid grid-cols-2 lg:grid-cols-4 gap-2">
       <div
       v-for="image in images"
       >
         <nuxt-img
           :src="`/images/${image}`"
+          loading="lazy"
+        />
+      </div>
+    </div>
+    <hr class="mt-4">
+    <h2 class="text-xl mt-8">Workaround</h2>
+    <p>This images works locally and on netlify but requre to set environment variable IMAGES_URL. On netlify image source is:</p>
+      <pre class="bg-gray-800 p-4 my-4 rounded text-white">
+        e.g. src="/_ipx/_/../../images/1.jpg"
+      </pre>
+    <div class="grid grid-cols-2 lg:grid-cols-4 gap-2">
+      <div
+      v-for="image in images"
+      >
+        <nuxt-img
+          :src="`${config.imagesUrl}/images/${image}`"
           loading="lazy"
         />
       </div>
